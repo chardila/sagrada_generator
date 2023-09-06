67196f788a9e3cf0589785314dd1ffb67e77f59b
Carlos Ardila <carlos@ardila.com.co>
2023-09-06 16:15:55 -0500

    upload generated images with and update card.txt file. Contains all the expansions until 2023

--- sagrada_generator.py
+++ sagrada_generator.py
@@ -1,25 +1,20 @@
-# https://boardgamegeek.com/filepage/219948/sagrada-card-generator
-
-# https://www.geeksforgeeks.org/working-images-python/
-# https://www.geeksforgeeks.org/adding-text-on-image-using-python-pil/'
-# https://stackoverflow.com/questions/8376359/how-to-create-a-transparent-gif-or-png-with-pil-python-imaging
-
 from PIL import Image, ImageDraw, ImageFont
 import os
 
 
-def createCard(label, parameter_file):
+def create_card(label, parameter_file):
+    # Load the font and create a blank image
     font = ImageFont.truetype('UncialAntiqua-Regular.ttf', 42)
     img = Image.new('RGB', (1055, 934), color='black')
 
-    text = label.strip()
+    text = label.strip()  # Clean up label
     print(text)
     total_balls = int(parameter_file.readline())
 
     for row in range(4):
-        rowline = parameter_file.readline()
+        row_line = parameter_file.readline().strip()  # Clean up row line
         for column in range(5):
-            tile_image = Image.open(rowline[column] + ".png")
+            tile_image = Image.open(row_line[column] + ".png")
             (height, width) = tile_image.size
             pos = (25 * (column + 1) + width * column, 20 * (row + 1) + height * row)
             img.paste(tile_image, pos)
@@ -30,27 +25,23 @@ def createCard(label, parameter_file):
         img.paste(ball, (995 - n_ball * (ball_size[0] + 6), 820))
 
     file_card_name = parameter_file.readline().strip()
-    pathCard = "sagrada_output/" + file_card_name + ".png"
-    kanvas = ImageDraw.Draw(img)
-    _, _, w, h = kanvas.textbbox((0, 0), text, font=font)
-    kanvas.text(((1055 - w) / 2, 810), text, font=font, fill='white')
+    path_card = os.path.join("sagrada_output", file_card_name + ".png")  # Use os.path.join for paths
+    canvas = ImageDraw.Draw(img)
+    _, _, w, h = canvas.textbbox((0, 0), text, font=font)
+    canvas.text(((1055 - w) / 2, 810), text, font=font, fill='white')
 
     img = img.resize((1063, 945), Image.LANCZOS)
-    img.save(pathCard, dpi=(300, 300))
-    label = parameter_file.readline()
-    if not label:
-        return
-    else:
-        createCard(label, parameter_file)
+    img.save(path_card, dpi=(300, 300))
+    next_label = parameter_file.readline().strip()
+
+    if next_label:
+        create_card(next_label, parameter_file)
 
 
 if __name__ == '__main__':
-
-    file = open('card.txt')
-    try:
+    # Use a context manager (with statement) to open and close the file automatically
+    with open('card.txt') as file:
         if not os.path.exists('sagrada_output'):
             os.mkdir('sagrada_output')
-        createCard(file.readline(), file)
-        print("√ finish")
-    finally:
-        file.close()
+        create_card(file.readline(), file)
+        print("✓ Finished")
